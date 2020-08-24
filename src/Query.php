e7b1c7f845f8a6c4b3d6f1664f5b821b5121174f
Jonas Silva <jonasribeiro19@gmail.com>
2020-08-24 06:02:12 -0300

    Connection, Table, Commons

---
+++ src/Query.php
@@ -0,0 +1,84 @@
+<?php
+namespace Bmodel;
+
+use Bmodel\Connection;
+
+class Query {
+  static $queryString = '';
+  /**
+   * Begin transaction
+   *
+   * @param String|Int $connectionsId Id da conexao
+   *
+   * @return Boolean
+   * @author Jonas Ribeiro <jonasribeiro19@gmail.com>
+   * @version 1.0
+   */
+  static public function beginTransaction($connectionsId = null)
+  {
+      $pdo = Connection::connect($connectionsId);
+      return $pdo->beginTransaction();
+  }
+  /**
+   * Commit transaction
+   *
+   * @param String|Tnt $connectionsId Id da conexao
+   *
+   * @return Boolean
+   * @author Jonas Ribeiro <jonasribeiro19@gmail.com>
+   * @version 1.0
+   */
+  static public function commit($connectionsId = null)
+  {
+      $pdo = Connection::connect($connectionsId);
+      return $pdo->commit();
+  }
+  /**
+   * Rollback transaction
+   *
+   * @param String|Int $connectionsId Id da conexao
+   *
+   * @return Boolean
+   * @author Jonas Ribeiro <jonasribeiro19@gmail.com>
+   * @version 1.0
+   */
+  static public function rollBack($connectionsId = null)
+  {
+      $pdo = Connection::connect($connectionsId);
+      return $pdo->rollBack();
+  }
+  /** Execulta query sql
+   *
+   * @param string $sql
+   * @param array $bindData Array com os valores para o bind.
+   * @param int $connectionsId Id da connection DEFAULT=NULL
+   * @throws \Exception
+   * @return boolean|\PDOStatement
+   */
+  static public function query($sql,$bindData = null,$connectionsId = null)
+  {
+      $pdo = static::connect($connectionsId);
+      if(is_null($bindData) || empty($bindData)){
+          self::$queryString = $sql;
+          if(!$query = $pdo->query($sql)){
+              list($handle, $codError, $StrError) = $pdo->errorInfo();
+
+              throw new \Exception("Error: #{$codError}: {$StrError}<br />\r\n".$sql,$codError);
+              return false;
+          }
+      } else {
+          $query = $pdo->prepare($sql);
+          if(!$query->execute( $bindData )){
+              list($handle, $codError, $StrError) = $query->errorInfo();
+              self::$queryString = $query->queryString;
+
+              throw new \Exception("Error: #{$codError}: {$StrError}<br />\r\n", $codError);
+              return false;
+          }
+          self::$queryString = $query->queryString;
+      }
+
+      return $query;
+  }
+
+}
